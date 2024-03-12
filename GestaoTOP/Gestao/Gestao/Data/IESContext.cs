ec5e3cee3bb9bcbd59ac5854be62e39bf6303d12
Thiago Campos <thiago-kono@hotmail.com>
2024-03-11 21:40:58 -0300

    Primerio commit

---
+++ GestaoTOP/Gestao/Gestao/Data/IESContext.cs
@@ -0,0 +1,28 @@
+﻿using Gestao.Models;
+using Gestao.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao.Data
+{
+    //CLASSE IESContext HERDA O DBCONTEXT DO EFCORE
+
+    public class IESContext : IdentityDbContext
+    {
+        //METODO CONSTRUTOR PARA GERENCIAMENTO DE DEPENDENCIA; PREPARA O CONTEXTO DO BANCO DE DADOS
+        //PARA QUE AS CLASSES ABAIXO SEJAM TRATADAS PARA QUE SE TORNEM UMA TABELA NO BANCO
+
+        public IESContext(DbContextOptions<IESContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Veiculo> Veiculo { get; set; }
+        public DbSet<Morador> Morador { get; set; }
+        public DbSet<Visitante> Visitante { get; set; }
+
+        public DbSet<Reservas> Reservas { get; set; }
+
+
+    }
+}
