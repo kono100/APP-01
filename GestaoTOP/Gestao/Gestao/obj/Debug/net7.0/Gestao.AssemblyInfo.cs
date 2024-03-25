d1ad14dd54e35d8d40d592091aecc10a5e941ea5
Thiago Campos <thiago-kono@hotmail.com>
2024-03-25 20:35:54 -0300

    add função Role

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2790f3d34103a20bd4f51426d37da3d3f805d31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5b02541ff5e2f17700d9eaa6ce04bfdd9422473")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
