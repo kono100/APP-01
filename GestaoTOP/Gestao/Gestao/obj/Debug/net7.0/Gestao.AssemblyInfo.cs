41fbc2d8302389f363a3909ea5744edfce8a4a9e
Thiago Campos <thiago-kono@hotmail.com>
2024-04-02 00:14:26 -0300

    ajuste na permissoes de acesso

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4af55531e808515a90f2e38915848f50846220")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8efdd5598414389cfd05e7c496aca8f2501b2455")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
