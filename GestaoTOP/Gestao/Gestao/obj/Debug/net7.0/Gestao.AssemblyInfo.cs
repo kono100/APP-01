2d62d2b684634dc3d0a692f58dd3d1859122cc92
Thiago Campos <thiago-kono@hotmail.com>
2024-04-04 16:36:35 -0300

    corrigido botao e ocultar e add alert de de permissao

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c54f8c71dd8901bc08ffe4b027227c5f113c814d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+782b0c16c144f9d61c1a74cc9aaddc6e87c8404b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
