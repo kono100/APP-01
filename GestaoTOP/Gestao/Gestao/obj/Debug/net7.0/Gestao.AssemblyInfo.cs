5484b5c8e55783439cf69b0465057713daeeffc5
Thiago Campos <thiago-kono@hotmail.com>
2024-04-06 13:42:13 -0300

    add função de alerta e bloqueio de datas não permitidas

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fdfe72883a13967fbe1abed4d8cdbfc410c267")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3380d308f71f4062981f9fc049e1b116aa3c9b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
