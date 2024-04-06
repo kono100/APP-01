13baa97af5d6593642f91ad0f56c300d9b69f1d0
Thiago Campos <thiago-kono@hotmail.com>
2024-04-06 17:42:25 -0300

    corrigido o bug dos checkbox exibir informações

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3380d308f71f4062981f9fc049e1b116aa3c9b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5484b5c8e55783439cf69b0465057713daeeffc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
