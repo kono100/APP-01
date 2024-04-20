94ebe2be15ca266e9dc885fc3b7728acd478cfd8
Thiago Campos <thiago-kono@hotmail.com>
2024-04-20 00:18:54 -0300

    add parte crud editar das informacoes api cep

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f808ab3f366f61154ffff8a7ffe104aa430435")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2431745b86512a1cccff76c621e91c1c2bdd76ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
