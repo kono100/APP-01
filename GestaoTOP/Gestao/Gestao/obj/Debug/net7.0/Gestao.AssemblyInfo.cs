a5a05e747c42ce7247fc15c56c01f832a6717abb
Thiago Campos <thiago-kono@hotmail.com>
2024-04-23 23:33:17 -0300

    add função de remove nome do meio e deixa primeiro e ultimo nome e correção de erro de digitação

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7064bd504775d0f85dfc66ce40b9f4f303797e14")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8532e741f97dca3b1298692dfa91a5fb9bf84cc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
