80fe0464a1986e92ebf373e60e3d0f3509a2c86a
Thiago Campos <thiago-kono@hotmail.com>
2024-04-25 11:51:43 -0300

    add função semelhante ao de Terca Hr e Min

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1f9dda85f57ddae0c1d921cc2c9df8210f42b75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19fff5228ebe5148015ef350337fad4fbc8d5307")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
