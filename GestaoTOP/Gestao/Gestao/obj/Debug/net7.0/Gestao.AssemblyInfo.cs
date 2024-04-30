33f223b982191ae79cc7bb33e2f103030428c7c3
Thiago Campos <thiago-kono@hotmail.com>
2024-04-30 17:53:26 -0300

    add idade e saudação de feliz aniversario se dia e mes igual a de hoje

--- GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
+++ GestaoTOP/Gestao/Gestao/obj/Debug/net7.0/Gestao.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe94aa64020d6cb4445fd692c4b2b5c9c3a92bd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d745db27a67f4efd10aa7a219a61541ed1fee085")]
 [assembly: System.Reflection.AssemblyProductAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Gestao")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
